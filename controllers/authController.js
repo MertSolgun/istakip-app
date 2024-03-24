5a5dbb866a72743aa3e54a5b89589090b6304107
MertSolgun <solgunmert@gmail.com>
2024-03-25 02:58:43 +0300

    fead:frontValidationOk

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,24 @@
 const userSchema = require("../models/user");
+const jtw = require("jsonwebtoken");
+require("dotenv").config();
+
+//JTW
+const maxAGE = 3 * 24 * 60 * 60 * 1000;
+const createToken = (id) => {
+  return jtw.sign({ id }, process.env.SECRET_KEY, {
+    expiresIn: maxAGE,
+  });
+};
+
+//ErrorHandler
+
+const errorHandler = (err) => {
+  let errors = { email: "", parola: "" };
+  if (err.code === 11000) {
+    errors.email = "This email address is already registered.";
+    return errors;
+  }
+};
 
 module.exports = {
   signup_get: (req, res) => {
@@ -11,10 +31,15 @@ module.exports = {
     const { email, parola } = req.body;
     try {
       const user = await userSchema.create({ email, parola });
+      const token = createToken(user._id);
+      res.cookie("jwt", token, { httpOnly: true, maxAGE: maxAGE });
       res.status(201).send({
+        error: false,
         user,
       });
     } catch (error) {
+      const errors = errorHandler(error);
+      res.status(400).json({ errors });
       ///
     }
   },
