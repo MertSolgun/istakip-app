e99c1bd33e597991df15b897d6b9d4fbe2cec069
MertSolgun <solgunmert@gmail.com>
2024-03-24 20:44:40 +0300

    fead:project-start

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ require("dotenv").config();
 
 //middlewares
 app.use(express.static("public"));
-
 app.set("view engine", "ejs");
 
+//Db connection
 const dbURI = process.env.dbURI;
-
 mongoose
   .connect(dbURI)
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+  .then((result) =>
+    app.listen(3000, () => {
+      console.log("Db connection");
+    })
+  )
+  .catch((err) => console.log(err, "Db not connection"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/works", (req, res) => {
+  res.render("works");
+});
